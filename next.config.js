8dece67a880ecf103d150cddc764c8680c85a9fb
天真 <qingweiSun@outlook.com>
2023-04-16 20:58:02 +0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// }
+//
+// module.exports = nextConfig
+
+const runtimeCaching = require("next-pwa/cache");
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+});
+module.exports = nextConfig;
