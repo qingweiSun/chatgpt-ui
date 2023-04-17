4a3474f1539d43b21205127d30426d8fbcaa9c32
天真 <qingweiSun@outlook.com>
2023-04-17 16:18:29 +0800

    test

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,65 +1,62 @@
-import { createParser } from "eventsource-parser";
-import { NextRequest } from "next/server";
+import {createParser} from "eventsource-parser";
+import {NextRequest} from "next/server";
 
 async function createStream(req: NextRequest) {
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
-  const body = await req.json();
-  const messages = body.messages;
-  const temperature = body.temperature ?? "1";
-  const presencePenalty = body.presencePenalty ?? "0";
-  const maxTokens = body.maxTokens || "";
-  const apiKey =
-    body.apiKey || "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";
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    method: "POST",
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages,
-      stream: true,
-    }),
-  });
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+    const body = await req.json();
+    const messages = body.messages;
+    const apiKey =
+        body.apiKey || "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";
+    const res = await fetch("https://api.openai.com/v1/chat/completions", {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages,
+            stream: true,
+        }),
+    });
 
-  return new ReadableStream({
-    async start(controller) {
-      function onParse(event: any) {
-        if (event.type === "event") {
-          const data = event.data;
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta.content;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            controller.error(e);
-          }
-        }
-      }
+    return new ReadableStream({
+        async start(controller) {
+            function onParse(event: any) {
+                if (event.type === "event") {
+                    const data = event.data;
+                    if (data === "[DONE]") {
+                        controller.close();
+                        return;
+                    }
+                    try {
+                        const json = JSON.parse(data);
+                        const text = json.choices[0].delta.content;
+                        const queue = encoder.encode(text);
+                        controller.enqueue(queue);
+                    } catch (e) {
+                        controller.error(e);
+                    }
+                }
+            }
 
-      const parser = createParser(onParse);
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk, { stream: true }));
-      }
-    },
-  });
+            const parser = createParser(onParse);
+            for await (const chunk of res.body as any) {
+                parser.feed(decoder.decode(chunk, {stream: true}));
+            }
+        },
+    });
 }
 
 export async function POST(req: NextRequest) {
-  try {
-    const stream = await createStream(req);
-    return new Response(stream);
-  } catch (error) {
-    console.error("[Chat Stream]", error);
-    return new Response(
-      ["```json\n", JSON.stringify(error, null, "  "), "\n```"].join("")
-    );
-  }
+    try {
+        const stream = await createStream(req);
+        return new Response(stream);
+    } catch (error) {
+        console.error("[Chat Stream]", error);
+        return new Response(
+            ["```json\n", JSON.stringify(error, null, "  "), "\n```"].join("")
+        );
+    }
 }
--- src/app/pages/chat/api/generate.ts
+++ src/app/pages/chat/api/generate.ts
@@ -1,132 +1,129 @@
-import { ChatMessage } from "@/app/pages/chat";
+import {ChatMessage} from "@/app/pages/chat";
 
 let tempStatus = "";
 
 export async function generateMessage(
-  messages: ChatMessage[],
-  controller: AbortController,
-  setMessages: (messages: ChatMessage[]) => void
+    messages: ChatMessage[],
+    controller: AbortController,
+    setMessages: (messages: ChatMessage[]) => void
 ) {
-  const newMessages = [...messages];
-  newMessages.push({
-    data: {
-      role: "assistant",
-      content: "loading",
-    },
-    time: new Date().toLocaleString(),
-  });
-  setMessages(newMessages);
-  const url =
-    process.env.NODE_ENV === "production" ? "/api/chat" : "/api/hello";
-
-  let param = messages.filter(
-    (message) =>
-      message.data.content != "loading" &&
-      message.data.content.indexOf("请求已取消") == -1
-  );
-  tempStatus = "";
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        messages: param.map((message) => message.data),
-        apiKey: undefined,
-        temperature: "0.6",
-        presencePenalty: 0,
-        maxTokens: undefined,
-      }),
-      signal: controller.signal,
+    const newMessages = [...messages];
+    newMessages.push({
+        data: {
+            role: "assistant",
+            content: "loading",
+        },
+        time: new Date().toLocaleString(),
     });
+    setMessages(newMessages);
+    const url =
+        process.env.NODE_ENV === "production" ? "/api/chat" : "/api/hello";
 
-    if (response.ok) {
-      const data = response.body;
-      if (data) {
-        const reader = data.getReader();
-        const decoder = new TextDecoder("utf-8");
-        let done = false;
-        let currentAssistantMessage = "";
-        while (!done) {
-          const { value, done: readerDone } = await reader.read();
-          if (value) {
-            let char = decoder.decode(value);
-            if (char === "\n" && currentAssistantMessage.endsWith("\n")) {
-              continue;
+    let param = messages.filter(
+        (message) =>
+            message.data.content != "loading" &&
+            message.data.content.indexOf("请求已取消") == -1
+    );
+    tempStatus = "";
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                messages: param.map((message) => message.data),
+                apiKey: undefined
+            }),
+            signal: controller.signal,
+        });
+
+        if (response.ok) {
+            const data = response.body;
+            if (data) {
+                const reader = data.getReader();
+                const decoder = new TextDecoder("utf-8");
+                let done = false;
+                let currentAssistantMessage = "";
+                while (!done) {
+                    const {value, done: readerDone} = await reader.read();
+                    if (value) {
+                        let char = decoder.decode(value);
+                        if (char === "\n" && currentAssistantMessage.endsWith("\n")) {
+                            continue;
+                        }
+                        if (char) {
+                            currentAssistantMessage = currentAssistantMessage + char;
+                            //替换最后一条消息
+                            newMessages.pop();
+                            //添加新消息
+                            newMessages.push({
+                                data: {
+                                    role: "assistant",
+                                    content: currentAssistantMessage,
+                                },
+                                time: new Date().toLocaleString(),
+                            });
+                            tempStatus = currentAssistantMessage;
+                            setMessages([...newMessages]);
+                        }
+                    }
+                    done = readerDone;
+                }
+            } else {
+                tempStatus = "data is null";
+                const newMessages = [...messages];
+                //替换最后一条消息
+                newMessages.pop();
+                //添加新消息
+                newMessages.push({
+                    data: {
+                        role: "assistant",
+                        content: "data is null",
+                    },
+                    time: new Date().toLocaleString(),
+                });
+                setMessages(newMessages);
             }
-            if (char) {
-              currentAssistantMessage = currentAssistantMessage + char;
-              //替换最后一条消息
-              newMessages.pop();
-              //添加新消息
-              newMessages.push({
+        } else {
+            tempStatus = response.statusText;
+            const newMessages = [...messages];
+            //替换最后一条消息
+            newMessages.pop();
+            //添加新消息
+            newMessages.push({
                 data: {
-                  role: "assistant",
-                  content: currentAssistantMessage,
+                    role: "assistant",
+                    content: response.statusText,
                 },
                 time: new Date().toLocaleString(),
-              });
-              tempStatus = currentAssistantMessage;
-              setMessages([...newMessages]);
-            }
-          }
-          done = readerDone;
+            });
+
+            setMessages(newMessages);
         }
-      } else {
-        tempStatus = "data is null";
-        const newMessages = [...messages];
+    } catch (e: any) {
+        if (e.name === "AbortError") {
+            if (
+                newMessages[newMessages.length - 1].data.content == "正在生成中..." &&
+                tempStatus == ""
+            ) {
+                setMessages(newMessages.slice(0, newMessages.length - 2));
+            } else {
+                newMessages[newMessages.length - 1].data.content =
+                    tempStatus + " (请求已取消)";
+                tempStatus = "";
+                setMessages(newMessages);
+            }
+            return;
+        }
         //替换最后一条消息
         newMessages.pop();
         //添加新消息
         newMessages.push({
-          data: {
-            role: "assistant",
-            content: "data is null",
-          },
-          time: new Date().toLocaleString(),
+            data: {
+                role: "assistant",
+                content: e.name === "AbortError" ? "请求已取消" : e.toString(),
+            },
+            time: new Date().toLocaleString(),
         });
         setMessages(newMessages);
-      }
-    } else {
-      tempStatus = response.statusText;
-      const newMessages = [...messages];
-      //替换最后一条消息
-      newMessages.pop();
-      //添加新消息
-      newMessages.push({
-        data: {
-          role: "assistant",
-          content: response.statusText,
-        },
-        time: new Date().toLocaleString(),
-      });
-
-      setMessages(newMessages);
     }
-  } catch (e: any) {
-    if (e.name === "AbortError") {
-      if (
-        newMessages[newMessages.length - 1].data.content == "正在生成中..." &&
-        tempStatus == ""
-      ) {
-        setMessages(newMessages.slice(0, newMessages.length - 2));
-      } else {
-        newMessages[newMessages.length - 1].data.content =
-          tempStatus + " (请求已取消)";
-        tempStatus = "";
-        setMessages(newMessages);
-      }
-      return;
-    }
-    //替换最后一条消息
-    newMessages.pop();
-    //添加新消息
-    newMessages.push({
-      data: {
-        role: "assistant",
-        content: e.name === "AbortError" ? "请求已取消" : e.toString(),
-      },
-      time: new Date().toLocaleString(),
-    });
-    setMessages(newMessages);
-  }
 }
