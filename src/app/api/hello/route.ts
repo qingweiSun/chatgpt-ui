9970004db7ba26f365e2ff4ce4c22ba80f17597c
天真 <qingweiSun@outlook.com>
2023-04-17 18:14:00 +0800

    test

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,8 @@
 export async function POST(request: Request) {
-  return fetch("https://www.qingwei.icu/api/generate", {
-    method: "POST",
-    body: request.body,
-  });
+
+    const res = await fetch("https://www.qingwei.icu/api/generate", {
+        method: "POST",
+        body: request.body,
+    })
+    return new Response(res.body);
 }
