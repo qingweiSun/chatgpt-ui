f163a8171564d7a9d5087dd165cbc4c87ab27a1f
天真 <qingweiSun@outlook.com>
2023-04-28 19:09:08 +0800

    up

--- src/app/components/slider/index.tsx
+++ src/app/components/slider/index.tsx
@@ -126,7 +126,6 @@ export default function Slider(props: {
                     });
                   }
                 }
-              } else {
                 setId({
                   id: tempList[tempList.length - 1].id,
                 });
