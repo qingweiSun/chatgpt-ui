9c5d1849bbab7688ba08491dad4120e8bc9e957e
天真 <qingweiSun@outlook.com>
2023-05-08 17:14:07 +0800

    修复

--- src/app/api/gpt/route.ts
+++ src/app/api/gpt/route.ts
@@ -1,11 +1,11 @@
-import fetch from "node-fetch";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
   const messages = body.messages;
   const apiKey =
     body.apiKey || "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";
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const api = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`,
@@ -17,5 +17,8 @@ export async function POST(request: Request) {
       stream: false,
     }),
   });
+  const res = new NextResponse(api.body);
+  res.headers.set("Content-Type", "application/json");
+  res.headers.set("Cache-Control", "no-cache");
   return res;
 }
