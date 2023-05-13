a70cbef0330ad7e00ef25e0fe12c127e43116874
天真 <qingweiSun@outlook.com>
2023-05-13 10:20:43 +0800

    update

--- src/app/components/Reward.tsx
+++ src/app/components/Reward.tsx
@@ -9,43 +9,62 @@ import { copyToClipboard } from "../pages/chat/markdown-text";
 export default function RewardView() {
   const { isMobile } = useContext(context);
   const isDarkMode = useMediaQuery({ query: "(prefers-color-scheme: dark)" });
-
+  const [isElectron, setIsElectron] = useState(
+    typeof navigator !== "undefined" &&
+      navigator.userAgent.indexOf("Electron") !== -1
+  );
   const [rewardModalOpen, setRewardModalOpen] = useState<boolean>(false);
+
+  function showPop() {
+    notification.info({
+      message: "需要自建吗？",
+      duration: 0,
+      description: (
+        <div
+          style={{
+            WebkitUserSelect: "text",
+          }}
+        >
+          随着本站用户量的增长，成本也在逐渐增高，如果你需要更好的体验，我可以帮你自建，12美元/月的消费就可以支持到几十个人使用(
+          12
+          美元不是给我的)，均摊下来很便宜，你可以给别人付费使用，如果你有意请加微信：
+          <a
+            onClick={() => {
+              copyToClipboard("18300240232");
+            }}
+          >
+            18300240232
+          </a>
+          ，如果你想更好的使用本站，也欢迎支持一下。另外，请不要在微信内打开
+          <Image
+            preview={false}
+            src={"./IMG_1300.jpg"}
+            alt={"收款码"}
+            style={{ flex: 1, width: "100%", borderRadius: 32 }}
+          />
+        </div>
+      ),
+    });
+  }
 
   useEffect(() => {
     if (rewardModalOpen) {
       setRewardModalOpen(false);
-      notification.info({
-        message: "需要自建吗？",
-        duration: 0,
-        description: (
-          <div
-            style={{
-              WebkitUserSelect: "text",
-            }}
-          >
-            随着本站用户量的增长，成本也在逐渐增高，如果你需要更好的体验，我可以帮你自建，12美元/月的消费就可以支持到几十个人使用(
-            12
-            美元不是给我的)，均摊下来很便宜，你可以给别人付费使用，如果你有意请加微信：
-            <a
-              onClick={() => {
-                copyToClipboard("18300240232");
-              }}
-            >
-              18300240232
-            </a>
-            ，如果你想更好的使用本站，也欢迎支持一下。另外，请不要在微信内打开
-            <Image
-              preview={false}
-              src={"./IMG_1300.jpg"}
-              alt={"收款码"}
-              style={{ flex: 1, width: "100%", borderRadius: 32 }}
-            />
-          </div>
-        ),
-      });
+      showPop();
     }
   }, [rewardModalOpen]);
+
+  useEffect(() => {
+    if (!isElectron) {
+      //每天触发一次
+      const lastShowTime = localStorage.getItem("show_notification_time") ?? "";
+      const now = new Date().toLocaleDateString();
+      if (lastShowTime != now) {
+        localStorage.setItem("show_notification_time", now);
+        setRewardModalOpen(true);
+      }
+    }
+  }, []);
   return (
     <ConfigProvider
       theme={{
